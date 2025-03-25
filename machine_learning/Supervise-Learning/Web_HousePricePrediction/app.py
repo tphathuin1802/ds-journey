ebb815062cd9c9bfd0511e8426f09683f2a2ddb7
Phat Huynh <phathuynhwork18@gmail.com>
2025-03-25 22:08:56 +0700

    commit my repository

--- machine_learning/Supervise-Learning/Web_HousePricePrediction/app.py
+++
@@ -1,5 +0,0 @@
-# Module pickle is use to centralize model to bit format with the achive mission is compile to training model in machine- learning
-
-import pickle
-class FileUtil:
-    def savemodel(model,file)
